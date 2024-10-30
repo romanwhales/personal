1938a6f553fd5e9077f08bd3d8e7ab99e6236559
Adedeji Olawale <adedejiolawale@Adedejis-MacBook-Pro.local>
2024-10-29 21:37:03 -0400

    Add database logging

--- app/index-ws.js
+++ app/index-ws.js
@@ -12,10 +12,21 @@ server.listen(3000,function (){
     console.log("Server started on port 3000")
 })
 
+
 /**Web socket */
 const WebSocketServer = require("ws").Server;
 
 const wss = new WebSocketServer({server:server});
+
+process.on("SIGINT",() => {
+    console.log("sigint")
+    wss.clients.forEach(function each(client){
+        client.close();
+    })
+    server.close(() => {
+        shutdownDB();
+    })
+})
 
 wss.on("connection",function connection(ws){
     const numClients = wss.clients.size;
@@ -25,6 +36,10 @@ wss.on("connection",function connection(ws){
     if(ws.readyState === ws.OPEN){
         ws.send("Welcome to my server");
     }
+
+    db.run(`INSERT INTO visitors (count,time)
+        VALUES (${numClients}, datetime('now'))
+    `)
     ws.on("close",function close(){
         wss.broadcast(`Current visitors: ${numClients}`)
         console.log("A client has diconnected")
@@ -36,4 +51,32 @@ wss.broadcast = function broadcast(data){
     wss.clients.forEach(function each (client){
         client.send(data);
     })
+}
+
+/** End websocket */
+
+/** Begin Database */
+
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE visitors (
+            count INTEGER, 
+            time TEXT  
+        )
+    `)
+})
+
+function getCounts(){
+    db.each("select * from visitors",(err, row) => {
+        console.log(row);
+    })
+}
+
+function shutdownDB(){
+    getCounts();
+    console.log("Shutting Down DB...")
+    db.close()
 }
