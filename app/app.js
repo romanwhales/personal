a903c80644528d693f5933dddb1083e17b227a36
Adedeji Olawale <adedejiolawale@Adedejis-MacBook-Pro.local>
2024-12-07 11:13:02 -0500

    update

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 http.createServer(function(req,res){
-	res.write("On the way to being a Fullstack Engineer!");
+	res.write("My name is Olawale, I am a Senior Software Engineer");
 	res.end();
 	
 }).listen(3000);
