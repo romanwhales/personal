4c14758968bd85a202756f1b21a426411e6d545a
Adedeji Olawale <adedejiolawale@Adedejis-MacBook-Pro.local>
2025-02-14 20:43:13 -0500

    upgrade

--- app/app.js
+++ app/app.js
@@ -1,8 +1,28 @@
-const http = require("http");
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000
 
-http.createServer(function(req,res){
-	res.write("My name is Olawale, I am a Senior Software Engineer");
-	res.end();
-	
-}).listen(3000);
+app.use(express.static('public'))
+// Without middleware
+app.get('/', function (req, res) {
+    const options = {
+        root: path.join(__dirname)
+    };
+
+    const fileName = 'index.html';
+    res.sendFile(fileName, options, function (err) {
+        if (err) {
+            console.error('Error sending file:', err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
+});
+
+app.listen(PORT, function (err) {
+    if (err) console.error(err);
+    console.log("Server listening on PORT", PORT);
+});
+
 console.log("Server started on port 3000");
